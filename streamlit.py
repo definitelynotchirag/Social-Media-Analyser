775475a9dcd4d11db75364e09bb72b20b70fa06b
chirag <chiragdave1888@gmail.com>
2025-01-10 15:50:47 +0530

    Remove unused dependencies and comment out custom CSS styling in streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -86,8 +86,8 @@ load_dotenv(".env")
 
 # Custom CSS for styling
 
-hide_decoration_bar_style = ''' <style> header {visibility: hidden;} </style> ''' 
-st.markdown(hide_decoration_bar_style, unsafe_allow_html=True) 
+# hide_decoration_bar_style = ''' <style> header {visibility: hidden;} </style> ''' 
+# st.markdown(hide_decoration_bar_style, unsafe_allow_html=True) 
 
 st.markdown("""
 <style>
